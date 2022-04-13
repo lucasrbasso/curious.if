8bd984c06b93c6157ba05a340ae9b9d38a572ff0
adriandamiao <79238503+AdrianDamiao@users.noreply.github.com>
2022-04-13 12:21:51 -0300

    Create Project using NestJS

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-global['fetch'] = require('node-fetch');
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
