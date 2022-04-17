613a819fa507406e4f3c6447b38b75e6a8f1b20f
adriandamiao <79238503+AdrianDamiao@users.noreply.github.com>
2022-04-17 18:46:09 -0300

    Adds an ErrorHandlingFilter for exception handling

---
+++ api/src/errors/error-handling-filter.ts
@@ -0,0 +1,27 @@
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus } from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+
+@Catch()
+export class ErrorHandlingFilter implements ExceptionFilter {
+  catch(exception: any, host: ArgumentsHost): any {
+    const context = host.switchToHttp();
+    const response = context.getResponse();
+
+    const statusCode = exception instanceof HttpException ? exception.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
+    
+    if(exception instanceof PrismaClientKnownRequestError) {      
+      response.status(statusCode).json({
+        statusCode,
+        message: `Prisma Error - ${exception.code} ${exception.meta['cause'] ? '- ' + exception.meta['cause'] : ''}`,
+      });
+    }
+    
+    const message = exception instanceof HttpException ?  exception.message || exception.message: 'Internal server error';
+    const errorResponse: any = {
+      statusCode,
+      message,
+    };
+
+    response.status(statusCode).json(errorResponse);
+  }
+}
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaService } from './database/prisma/prisma.service';
+import { ErrorHandlingFilter } from './errors/error-handling-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new ErrorHandlingFilter());
+
   const prismaService = app.get(PrismaService);
   await app.listen(3000);
   await prismaService.enableShutdownHooks(app);
