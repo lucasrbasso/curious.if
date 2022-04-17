42e51ab65435ebd36289396ad508c2ea38a895c0
adriandamiao <79238503+AdrianDamiao@users.noreply.github.com>
2022-04-17 18:47:27 -0300

    Refactors initial migrations

--- api/prisma/migrations/20220414004400_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Post" (
-    "id" SERIAL NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "textMessage" VARCHAR(280) NOT NULL,
-    "authorId" INTEGER NOT NULL,
-
-    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
-);
