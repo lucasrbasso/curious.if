5c2104e9cd8773356a5d39c45fd620bf57858819
unknown <gustavoc.moraes@hotmail.com>
2022-05-15 22:19:50 -0300

    fix: fixed merge conflicts
    Co-Authored-By: Luan <9398249+luanvenancio@users.noreply.github.com>
    Co-Authored-By: Lucas Basso <lucas.rbasso@outlook.com>
    Co-Authored-By: Adrian Damião <79238503+AdrianDamiao@users.noreply.github.com>

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,11 +3,6 @@ import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 import { PostModule } from './post/post.module';
 import { AuthModule } from './auth/auth.module';
-<<<<<<< HEAD
-import { RolesGuard } from './users/roles/role.guard';
-import { APP_GUARD } from '@nestjs/core';
-=======
->>>>>>> 8a92927b268ada7b14101e1c4b3f59ca279180ca
 
 @Module({
   imports: [DatabaseModule, UsersModule, PostModule, AuthModule],
