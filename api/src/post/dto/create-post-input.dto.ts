05d2d46e99af9b520e7f9105e39bdd5bbc6bcdcd
unknown <gustavoc.moraes@hotmail.com>
2022-05-15 21:59:13 -0300

    feat: added field 'to' at post, returning user data on login, route to change roles and permissions, id is not more required as param on create post
    Co-Authored-By: Luan <9398249+luanvenancio@users.noreply.github.com>
    Co-Authored-By: Lucas Basso <lucas.rbasso@outlook.com>
    Co-Authored-By: Adrian Damião <79238503+AdrianDamiao@users.noreply.github.com>

--- api/src/post/dto/create-post-input.dto.ts
+++ api/src/post/dto/create-post-input.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString, IsUUID, IsOptional } from 'class-validator';
+import { IsNotEmpty, IsString, IsOptional } from 'class-validator';
 
 export class CreatePostInputDTO {
   @ApiProperty()
